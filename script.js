ac0dcf421e0535005d9fa482e807be36fd0847e1
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-03-03 23:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const mapLetter = (str) =>{
+
+	const obj = {};
+
+	for(let i = 0; i < str.length; i++){
+		const char = str[i];
+		obj[char] = obj[char] || [];
+		obj[char].push(i);
+	}
+
+	return obj;
+}
+    
+const str = prompt("Enter the string");
+console.log(mapLetter(str.trim()));
